1c5f18b5646a3b7f1111cde28fe0d24b517a5364
Andy Roberts <andy.roberts@carpe-diem.co.uk>
2017-10-05 17:27:39 +0100

    Elasticsearch support

--- app/Services/ElasticsearchQuery.php
+++ app/Services/ElasticsearchQuery.php
@@ -15,14 +15,33 @@ class ElasticsearchQuery
         $this->client = $client; //we need to be able to search agianst elasticsearch
     }
 
-    public function search($term)
+    public function search($term, array $filters = [])
     {
         //build body with term
+        $this->buildQuery($term, $filters);
         $response = $this->client->search($index, $indexes, $body);
         // append the wheel data to the results
         $this->results = $response['hits']['hits'];
         //$this->aggregations = $this->applyAggregationSelections(collect($response['aggregations']));
         $this->totalRecords = $response['hits']['total'];
+    }
+
+    protected function buildQuery($term, array $filters)
+    {
+        $body['query']['bool']['must'] = [
+            'multi_match' => [
+                'query' => $term,
+                'type' => 'best_fields',
+                'fields' => [
+                    'name',
+                    'synopsis',
+                    'speakers',
+                    'sponsors',
+                    'keypoints'
+                ],
+                'operator' => 'and'
+            ]
+        ];
     }
 
     public function where()
