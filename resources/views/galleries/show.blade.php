c73140959c500925e0c28987f9c66053df4ad95b
Andy Roberts <andy.roberts@carpe-diem.co.uk>
2017-10-09 17:15:34 +0100

    Basic gallery page

--- resources/views/galleries/show.blade.php
+++ resources/views/galleries/show.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-
-    @foreach($gallery->media as $image)
-        <img src="{{ $image->getPresetPath('thumb') }}" class="rounded float-left" alt="sm">
-    @endforeach
-
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mb-5">{{ $gallery->name }}</h1>
+            </div>
+        </div>
+        @foreach($gallery->media->chunk(4) as $row)
+            <div class="row mb-5">
+                @foreach($row as $image)
+                    <div class="col-3">
+                        <a href="{{ $image->getPath() }}" target="_blank">
+                            <img src="{{ $image->getPresetPath('thumb') }}" class="rounded float-left" alt="sm" width="100%">
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @endforeach
+    </div>
 @stop
